2c9d92f834599ce607e850985c5c686755a2229c
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2021-06-10 22:15:01 +0700

    Menambah route untuk welcome

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 Route::get('/daftar', function () {
     return view('daftar');
 });
