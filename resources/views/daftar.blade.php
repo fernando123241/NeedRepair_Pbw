b1f264ee651f545adc03e0742a712b774dfe7183
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2021-06-10 22:14:36 +0700

    Melengkapi halaman daftar

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -1,1 +1,74 @@
-<h1>Hallo</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar | NeedRepair</title>
+    <style>
+    body {
+        padding: 0;
+        background : #59C2AF;
+        font-family : sans-serif;
+    }
+    .box h1{
+        color: white;
+        font-weight: bold;
+        font-size : 23px;
+    }
+    .box {
+        width: 300px;
+        padding: 10px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background : #59C2AF;;
+        text-align: center;
+    }
+    .box input[type="text"], .box input[type="password"], .box input[type="email"]{
+        border: 0;
+        background : white;
+        display: block;
+        margin: 20px auto;
+        text-align: left;
+        padding: 14px 10px;
+        width: 250px;
+        height : 20px;
+        color: black;
+        border-radius: 10px;
+    }
+    .box input[type="submit"]{
+        border: 0;
+        background: #5F5F5F;;
+        display: block;
+        margin: 20px auto;
+        text-align: center;
+        border: 2px solid #5F5F5F;
+        width: 120px;
+        height : 40px;
+        color: white;
+        border-radius: 10px;
+        cursor: pointer;
+        font-size: 20px;
+        font-weight: bold;
+    }
+    </style>
+</head>
+<body>
+    <nav class="navbar fixed-top navbar-light bg-light">
+        <div class="container-fluid">
+            <a href="{{ url('welcome') }}"><img src="img/back.png" alt="" style="width: 55px; margin-top: 5px; margin-left: 5px;"></a>
+            <img src ="img/LogoNama.png" alt="" width="180" height="55" style="float:right; margin-right:50px;">
+        </div>
+    </nav>
+    <form class="box" action="" method="post">
+        <img src="img/profil_putih.png" alt="" style="width: 85px; height: 85px;"><br><br>
+        <h1>Create Account</h1>
+        <input type="text" name="" placeholder="Name">
+        <input type="email" name="" placeholder="Email">
+        <input type="password" name="" placeholder="Password">
+        <input type="submit" name="" value="Create">
+    </form>
+</body>
+</html>
