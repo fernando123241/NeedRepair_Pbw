89398d2209645218dbb6bbf7a85752ff3573a942
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2021-06-16 23:22:24 +0700

    Membuat akun seeder

---
+++ database/seeders/AkunSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AkunSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $user = [
+        //     [
+        //         'name' => 'Fernando',
+        //         'email' => 'fernando@gmail.com',
+        //         'password' => bcrypt('adminadmin'),
+        //         'level' => 'admin',
+        //     ],
+        //     [
+        //         'name' => 'Adil',
+        //         'email' => 'adil@gmail.com',
+        //         'password' => bcrypt('12345678'),
+        //         'level' => 'admin',
+        //     ],
+        //     [
+        //         'name' => 'Faris',
+        //         'email' => 'faris@gmail.com',
+        //         'password' => bcrypt('admin12345'),
+        //         'level' => 'admin',
+        //     ]
+        //     ];
+        // foreach ($user as $key => $value) {
+        //         User::Create($value);
+        // }
+    }
+}
